af78bd02f0f3e7f28674fc82a51cea04771a9827
Kamil Kornek <kkornek@jasfbg.pl>
2023-08-23 22:43:09 +0200

    add files

--- PetSpace.Data/Repositories/Interfaces/IFoodInventoryRepository.cs
+++ PetSpace.Data/Repositories/Interfaces/IFoodInventoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PetSpace.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace PetSpace.Data.Repositories.Interfaces
 {
-    internal class IFoodInventoryRepository
+    public interface IFoodInventoryRepository
     {
+        Task<FoodInventory> GetFoodInventoryById(int inventoryId);
+        Task<double> GetTotalQuantityForUser(int userId);
+        Task Add(FoodInventory inventory);
+        void Update(FoodInventory inventory);
+        Task Delete(int inventoryId);
     }
 }
--- PetSpaceAPI/Models/Pets/PetDto.cs
+++ PetSpaceAPI/Models/Pets/PetDto.cs
@@ -1,6 +1,11 @@
 ﻿namespace PetSpaceAPI.Models.Pets
 {
-    public class Pet
+    public class PetDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string OwnerName { get; set; }
     }
 }
